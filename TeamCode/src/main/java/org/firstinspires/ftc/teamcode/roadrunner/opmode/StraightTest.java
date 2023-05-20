efd1bcabd0bdd7ad94c03af03e6fc913c80396ef
Harshiv15 <harshiv_puri@asl.org>
2023-05-20 11:11:20 +0100

    Working rr + drive + bruh I rly forgot how to use loops :|

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/StraightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/StraightTest.java
@@ -17,7 +17,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 @Config
 @Autonomous(group = "drive")
 public class StraightTest extends LinearOpMode {
-    public static double DISTANCE = 24; // in
+    public static double DISTANCE = 25; // in
 
     @Override
     public void runOpMode() throws InterruptedException {
